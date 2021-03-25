1e6a8b1641783c48002fed38785361c063cd5ecd
Jack hou <256595662@qq.com>
2021-03-25 11:22:09 +0800

    蓝桥杯练习

--- src/课程练习/迷宫2017.java
+++ src/课程练习/迷宫2017.java
@@ -50,12 +50,39 @@ public class 迷宫2017 {
     static boolean ans[][]=new boolean[10][10];
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            c[i] = s[i].toCharArray();
+            c[i] = s[i].toCharArray();//将字符串数组变成字符数组
         }
         for (int i = 0; i < 10; i++) {
-            System.out.println(Arrays.toString(c[i]));//将字符串数组变成字符数组
-               
+            System.out.println(Arrays.toString(c[i]));
 
         }
+        int fin=0;
+        for (int i = 0; i <10; i++) {
+            for (int j = 0; j <10 ; j++) {
+                boolean vis[][]=new boolean[10][10];
+                int x=i,y=j;
+                //判断是否在10*10圈内，和判断当前位置是否访问过,若访问过则变为true，！true就变成了false无法通过循环
+                while (x>=0&&x<10&&y>=0&&y<10&&!vis[x][y]){
+                    vis[x][y]=true;
+                    if (c[x][y]=='U'){
+                        x--;
+                    }
+                    else if (c[x][y]=='D'){
+                        x++;
+                    }
+                    else if (c[x][y]=='L'){
+                        y--;
+                    }
+                    else if (c[x][y]=='R'){
+                        y++;
+                    }
+                }
+                if (x<0||y<0||x>=10||y>=10){//假如上一步的循环中因为！true跳出则肯定还在圈内死循环，则次if条件也不满足也不会进行操作
+                    vis[i][j]=true;
+                    fin++;
+                }
+            }
+        }
+        System.out.println(fin);
     }
 }
