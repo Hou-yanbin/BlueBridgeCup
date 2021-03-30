83a1226ba9bf80b988a298e32f154435580a4f20
Jack hou <256595662@qq.com>
2021-03-30 11:46:04 +0800

    蓝桥杯练习

---
+++ src/课后习题/Step4容器/平方怪圈2016.java
@@ -0,0 +1,19 @@
+package 课后习题.Step4容器;
+
+/**
+ * @version v-1.8.0_131.
+ * @auther Jack hou  Email:256595662@qq.com
+ * @data 2021/3/30/11:45
+ * @Description:
+ **/
+/*
+题目描述
+如果把一个正整数的每一位都平方后再求和，得到一个新的正整数。对新产生的正整数再做同样的处理。
+如此一来，你会发现，不管开始取的是什么数字，最终如果不是落入1，就是落入同一个循环圈。
+请写出这个循环圈中最大的那个数字。
+ */
+public class 平方怪圈2016 {
+    public static void main(String[] args) {
+
+    }
+}
